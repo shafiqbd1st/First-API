fd9b9086b59037874b55005cd045f663f562e4e8
shafiqbd1st <shafiqbd1st.com>
2024-04-26 16:41:47 +0000

    Fri, Apr 26, 2024, 10:41 PM +06:00

--- life_care/settings.py
+++ life_care/settings.py
@@ -19,7 +19,7 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+environ.Env.read_env(env_file=str(BASE_DIR) + "/.env")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 

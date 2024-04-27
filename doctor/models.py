d31faccec8ece38a1b3fb3bdf591b97442fb4074
shafiqbd1st <shafiqbd1st.com>
2024-04-27 15:35:09 +0000

    Sat, Apr 27, 2024, 9:35 PM +06:00

--- doctor/models.py
+++ doctor/models.py
@@ -1,9 +1,6 @@
 from django.db import models
-
-# Create your models here.
 from django.contrib.auth.models import User
 from patient.models import Patient
-
 
 class Specialization(models.Model):
     name = models.CharField(max_length=100)
@@ -24,7 +21,6 @@ class AvailableTime(models.Model):
     
     def __str__(self):
         return self.time
-
 
 class Doctor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)

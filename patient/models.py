13ba05a96823957d48908f4f2322353296f1b10f
shafiqbd1st <shafiqbd1st.com>
2024-04-27 17:25:02 +0000

    Sat, Apr 27, 2024, 11:25 PM +06:00

--- patient/models.py
+++ patient/models.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='patient/images/')
-   
     mobile_no = models.CharField(max_length=12)
     
     def __str__(self):
